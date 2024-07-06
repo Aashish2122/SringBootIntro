41a00a801eab51cfa35272b1bc3b36bd817ed0da
Aashish Kumar Jha <ash1999.official@gmail.com>
2024-07-06 19:21:20 +0530

    Initial setup of Springboot project

---
+++ IntroductionProject/src/main/java/com/aashish/IntroductionProject/IntroductionProjectApplication.java
@@ -0,0 +1,13 @@
+package com.aashish.IntroductionProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionProjectApplication.class, args);
+	}
+
+}
---
+++ IntroductionProject/src/test/java/com/aashish/IntroductionProject/IntroductionProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aashish.IntroductionProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
